b2a10f79d9ba423f01afd8b1cf8aab78f1137939
Amitesh Thind <31372481+AmiteshThind@users.noreply.github.com>
2018-12-06 00:31:44 -0500

    Update PasswordGenerator_Arvi.py

--- PasswordGenerator_Arvi.py
+++ PasswordGenerator_Arvi.py
@@ -2,39 +2,25 @@ import random
 import string
 
 specialCharacters = "-|@.,?/!~#%^&*(){}[]\=*"
-
-specialCharacters = list(specialCharacters)
-
-sizeOfSpecialCharacters = len(specialCharacters)
-
-passwordLength = random.randint(11, 23)
-
-numLowercaseCharacters = random.randint(1, passwordLength - 3)
-
-numUppercaseCharacters = (random.randint(1, (passwordLength - numLowercaseCharacters) - 2))
-
-numSpecialCharacters = random.randint(1, (passwordLength - (numUppercaseCharacters + numLowercaseCharacters + 1)))
-
-numRandNumber = passwordLength - (numLowercaseCharacters + numUppercaseCharacters + numSpecialCharacters)
-
 password = ""
+specialCharacters = list(specialCharacters)
+sizeOfSpecialCharacters = len(specialCharacters)
+passwordLength = random.randint(11, 23)
+numLowercaseCharacters = random.randint(1, passwordLength - 3)
+numUppercaseCharacters = (random.randint(1, (passwordLength - numLowercaseCharacters) - 2))
+numSpecialCharacters = random.randint(1, (passwordLength - (numUppercaseCharacters + numLowercaseCharacters + 1)))
+numRandNumber = passwordLength - (numLowercaseCharacters + numUppercaseCharacters + numSpecialCharacters)
 
 for i in range(0, numLowercaseCharacters):
     password += random.choice(string.ascii_lowercase)
-
 for i in range(0, numUppercaseCharacters):
     password += random.choice(string.ascii_uppercase)
-
 for i in range(0, numSpecialCharacters):
     password += specialCharacters[random.randint(0, sizeOfSpecialCharacters)]
-
 for i in range(0, numRandNumber):
     password += str(random.randint(0, 9))
 
 passwordList = list(password)
-
 random.shuffle(passwordList)
-
 password = "".join(passwordList)
-
 print("Password:" + password)
